dc9fba053d8e742de8ca80372c4fdb7b3c7441ee
vedizzz2 <son491413@gmail.com>
2025-04-27 11:49:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -187,7 +187,7 @@ async def start(client:Client, message):
                             InlineKeyboardButton('Tᴏᴘ Tʀᴇɴᴅɪɴɢ ⚡', callback_data="trending")
                         ]] 
                         reply_markup = InlineKeyboardMarkup(buttons)
-                        m=await message.reply_sticker("CAACAgUAAxkBAAILE2gNxWvgA8ybeI-ht1N222aXprVRAAIEAAPBJDExieUdbguzyBAeBA") 
+                        m=await message.reply_sticker("CAACAgUAAxkBAAKr02gNzDnJzDYRrUnQkF-yKnHByUC2AAIEAAPBJDExieUdbguzyBA2BA") 
                         await asyncio.sleep(1)
                         await m.delete()
                         await message.reply_photo(photo=random.choice(START_IMG), caption=script.START_TXT.format(message.from_user.mention, get_status(), message.from_user.id),
